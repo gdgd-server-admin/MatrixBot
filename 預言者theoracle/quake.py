74b837504a41280a51dc321a4f5d7b4977e957f3
huxian123 <admin@gdgd.jp.net>
2023-01-12 11:24:01 +0900

    データの解釈方法を間違えていたので書き直し

--- 預言者theoracle/quake.py
+++ 預言者theoracle/quake.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 # 地震情報の取得
 
 # sqlite3を使用して「どこまで取得したか」を記録する
@@ -14,44 +15,38 @@ def get_data():
   cur = con.cursor() 
   cur.execute("CREATE TABLE IF NOT EXISTS HIST(id text,stamp integer)")
 
-  quake_feed = feedparser.parse("https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml")
+  quake_feed = feedparser.parse("https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml") # 気象庁の高頻度フィード（地震火山）を読み込む
 
   for entry in quake_feed.entries:
     id = entry.id
     sql_find_id = "select * from HIST where id='{}'".format(id)
     id_found = cur.execute(sql_find_id)
-    if id_found.fetchone() is None:
-#     print("履歴に存在しない")
-      now = time.time()
-      sql_insert_id = "insert into HIST values ('{}',{})".format(id, int(time.time()))
-      cur.execute(sql_insert_id)
-      child = feedparser.parse(id)
+    if id_found.fetchone() is None and str(entry.title).find("震源・震度に関する情報") >= 0: # タイトルで欲しい情報か判別する
       try:
-        for centry in child.entries:
-          code = centry["code"]
+        print("履歴に存在しない地震情報")
+        now = time.time()
+        sql_insert_id = "insert into HIST values ('{}',{})".format(id, int(time.time())) # 履歴に残す
+        cur.execute(sql_insert_id)
+        child = feedparser.parse(id) # idが詳細のURLになっているのでそれを読み込む
+        summary = child.feed.summary.split("\n") # 後続処理のために行単位で分割する
+        stamp = dateutil.parser.parse(summary[0]).strftime("%Y/%m/%d %H:%M") # 最初の１行目が報告時刻らしいのでそれを整形する
+        tmp = "<font color=\"gray\"><b>地震情報 【 {} 発生】</b><br/> <br/>　震源地： {} <br/> <br/>".format(stamp,summary[4]) # ５行目に震源地がちょうど来る（feedparserが偶然処理している）
+        tmp = tmp + "<table><tr><th>地域</th><th>震度</th></tr>" # 表にした方が見やすいと思うのでHTMLの表を作る
+        for s in summary:
+          if s.find("<area />") >= 0:
+            m = s.replace("<area />","") # <area />というタグが見える行に各地の震度が（偶然）乗る
+            if not m == "":
+              print(m)
+              area = m.split("<code>")[0] # データの中身を見ると<code>の手前までに地域名が見えるので切り取る
+              maxint = m.split("</code>")[1] # データの中身を見ると</code>の後に震度が見えるので切り取る
+              tmp = tmp + "<tr><td> {} </td><td> {} </td></tr>".format(area,maxint) # 行のHTMLにして繋げる
 
-          # 地震情報はcode=361
-          if code == "361" and child.feed.information["type"] == '震源・震度に関する情報（市町村等）':
-            # 震源と震度の情報
-            summary = child.feed.summary.split("\n")
-            stamp = dateutil.parser.parse(summary[0]).strftime("%Y/%m/%d %H:%M")
-            tmp = "<font color=\"gray\"><b>地震情報 【 {} 発生】</b><br/> <br/>　震源地： {} <br/> <br/>".format(stamp,summary[4])
-            tmp = tmp + "<table><tr><th>地域</th><th>震度</th></tr>"
-            for s in summary:
-              if s.find("<area />") >= 0:
-                m = s.replace("<area />","")
-                if not m == "":
-                  print(m)
-                  area = m.split("<code>")[0]
-                  maxint = m.split("</code>")[1]
-                  tmp = tmp + "<tr><td> {} </td><td> {} </td></tr>".format(area,maxint)
-
-            tmp = tmp + "</table> </font>"
-            msg.append(tmp)
+        tmp = tmp + "</table> </font>" # HTMLを閉じる
+        msg.append(tmp)
       except:
         pass
 
-#  cur.execute("delete from HIST where id=(select id from HIST order by stamp desc limit 1)")
-  con.commit()
+  #cur.execute("delete from HIST")
+  #con.commit()
 
   return set(msg)
