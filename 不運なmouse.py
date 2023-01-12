2431c80f8e3b8a3782a627ab9c0bd74cba24be63
huxian123 <admin@gdgd.jp.net>
2023-01-12 22:28:52 +0900

    さいころbotに説明を追加

--- 不運なmouse.py
+++ 不運なmouse.py
@@ -9,7 +9,7 @@ MATRIX_ROOM = "#off-topic:smith.gdgd.jp.net"
 
 MY_NAME = "@bottest:smith.gdgd.jp.net"
 
-
+# pip install matrix_client を実行してMatrix接続用のモジュールをインストールする
 from matrix_client.client import MatrixClient
 import random
 
@@ -20,17 +20,20 @@ room = client.join_room(MATRIX_ROOM)
 
 # 誰かが発言した時の反応
 def on_message(room, event):
-    if event['type'] == "m.room.message": # 部屋の中のメッセージ
-        if event['content']['msgtype'] == "m.text": # メッセージは文字で画像とか動画ではない
-            if not event['sender'] == MY_NAME:
-                if event['content']['body'] == "さいころ":
+    if event['type'] == "m.room.message":                               # 部屋の中のメッセージ
+        if event['content']['msgtype'] == "m.text":                     # メッセージは文字で画像とか動画ではない
+            if not event['sender'] == MY_NAME:                          # 送信者が自分「ではない」
+                if event['content']['body'] == "さいころ":               # 本文が「さいころ」である
                     room.send_text("[{}]".format(random.randint(1, 6))) # 1から6までのランダムな数字を発現する
 
 # 裏でメッセージを受け付ける
 room.add_listener(on_message)
 client.start_listener_thread()
 
+# 無限ループ
 while True:
-    string = input("文字列を入力してください:")
-    if string == "quit":
-        break
+    string = input("文字列を入力してください:") # 文字入力を受け付ける
+    if string == "quit":                    # 「quit」と入力されたら
+        break                               # ループを抜ける
+
+# プログラム終了
